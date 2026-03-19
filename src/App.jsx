eaf0d51dc39bcb611dcdcfb9d0381d7a64742d2b
Archit Patle <architpatle06@gmail.com>
2026-03-19 20:28:21 +0530

    Added ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import Navbar from "./components/layout/Navbar/Navbar";
 import Footer from "./components/layout/Footer/Footer";
 import AppRoutes from "./routes/AppRoutes";
 import './index.css'
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
   return (
     <div className="bg-bgPrimary min-h-screen text-white">
       
       <Navbar />
-
+<ScrollToTop />
       <main>
         <AppRoutes />
       </main>
